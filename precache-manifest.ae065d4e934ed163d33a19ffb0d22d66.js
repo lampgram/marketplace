25bc1715f3e15f1b45be0ec069cde6712e050aeb
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-08-14 17:40:18 +0300

    deployed by bot

---
+++ precache-manifest.ae065d4e934ed163d33a19ffb0d22d66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fd4e8697841a1325a5043e4eea2f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77bd75aeb9072746c55c",
+    "url": "/static/js/2.5d0314f1.chunk.js"
+  },
+  {
+    "revision": "eea3c963d11c5bf4422e",
+    "url": "/static/js/main.346ea570.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3bf05f8c976856fa976a1b2a3194a59",
+    "url": "/static/media/logo.d3bf05f8.png"
+  }
+]);
